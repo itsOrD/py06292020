7a747ab2a685609f617d20f5a7938b8b1ef7c006
itsOrD <matthew.beckerleg@gmail.com>
2020-06-30 16:29:28 -0400

    login counter established, regEx parsing in progress

---
+++ attemptLogin/keystone_counter.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+'''
+Author: itsOrD
+Description: count failed login attempts via keystone
+'''
+
+# import keystone.common.wsgi
+
+def main():
+
+    failed_login_counter = 0
+
+    k_file = open("/home/student/mycode/attemptLogin/keystone.common.wsgi", "r")
+
+    k_file_lines = k_file.readlines()
+
+    for line in k_file_lines:
+        if '- - - - -] Authorization failed' in line:
+            failed_login_counter += 1
+
+    print(f'''Failed logins currently total: {failedLoginCounter}''')
+
+
+    # same, but with regex
+    r_fail_count = 0
+
+    
+
+    print('''Via the mystical dark arts of regEx we can confirm there are "{r_fail_count}" failed logins''')
+
+
+if __name__ == "__main__":
+    main()
+
